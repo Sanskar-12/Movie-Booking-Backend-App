7a38f48a96b2de617a6dbb5182e52e6a73084a13
Sanskar-12 <sanskarv2004@gmail.com>
2026-01-01 09:50:59 +0530

    Feature: Added Theatre Model

---
+++ models/theatre.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    pincode: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Theatre = mongoose.model("Theatre", schema);
