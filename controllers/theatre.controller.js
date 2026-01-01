82ee8ef022461fbc88c2597744da3ffa5db391cf
Sanskar-12 <sanskarv2004@gmail.com>
2026-01-01 11:19:50 +0530

    Feature: Created Get Theatre by Id and Delete Theatre By Id api

--- controllers/theatre.controller.js
+++ controllers/theatre.controller.js
@@ -25,3 +25,57 @@ export const createTheatre = async (req, res) => {
     });
   }
 };
+
+export const deleteTheatre = async (req, res) => {
+  try {
+    const { theatreId } = req.params;
+
+    const theatre = await Theatre.findById(theatreId);
+
+    if (!theatre) {
+      return res.status(404).json({
+        success: false,
+        message: "Theatre not found",
+      });
+    }
+
+    await theatre.deleteOne();
+
+    return res.status(200).json({
+      success: true,
+      message: "Theatre deleted Successfully",
+    });
+  } catch (error) {
+    console.log("Error in deleteTheatre", error);
+    return res.status(500).json({
+      success: false,
+      message: `deleteTheatre Error: ${error}`,
+    });
+  }
+};
+
+export const getTheatre = async (req, res) => {
+  try {
+    const { theatreId } = req.params;
+
+    const theatre = await Theatre.findById(theatreId);
+
+    if (!theatre) {
+      return res.status(404).json({
+        success: false,
+        message: "Theatre not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      data: theatre,
+    });
+  } catch (error) {
+    console.log("Error in getTheatre", error);
+    return res.status(500).json({
+      success: false,
+      message: `getTheatre Error: ${error}`,
+    });
+  }
+};
--- routes/theatre.routes.js
+++ routes/theatre.routes.js
@@ -1,9 +1,15 @@
 import express from "express";
 import { validateCreateTheatreRequest } from "../middlewares/theatre.middleware.js";
-import { createTheatre } from "../controllers/theatre.controller.js";
+import {
+  createTheatre,
+  deleteTheatre,
+  getTheatre,
+} from "../controllers/theatre.controller.js";
 
 const theatreRouter = express.Router();
 
 theatreRouter.post(`/theatres`, validateCreateTheatreRequest, createTheatre);
+theatreRouter.get(`/theatres/:theatreId`, getTheatre);
+theatreRouter.delete(`/theatres/:theatreId`, deleteTheatre);
 
 export default theatreRouter;
